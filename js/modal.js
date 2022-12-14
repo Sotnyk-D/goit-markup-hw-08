f8ce6d29e12d234689f2c79fe25563015418cd96
Dmytro <112177564+Sotnyk-D@users.noreply.github.com>
2022-12-14 12:01:56 +0100

    update for mobile

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,16 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    //  scrollLock: document.querySelector("[data-hidden]"),
+   //  scrollLock: document.querySelector("[data-hidden]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+//   refs.scrollLock.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("hero__is-hidden");
+   //  refs.scrollLock.classList.toggle("overflow");
   }
 })();
